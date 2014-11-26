32a7ce5a177e3b5fdea6a11c2a75419677c86e6d
Thibaud Arnoux <thibaud.arnoux@lip6.fr>
2014-11-26 14:15:52 +0100

    Ajout outils d'analyse

--- extendclique.py
+++ extendclique.py
@@ -1,12 +1,18 @@
 import sys
-import CliqueCritique
-delta = int(sys.argv[1]) 
+from CliqueCritique import CliqueCritique
+from CliqueMaster import CliqueMaster
+from Clique import Clique
 
+delta = int(sys.argv[1])
+cliques=[]
 for line in sys.stdin:
-	contents = line.split(" ")
-	print contents[3]
-	if str(contents[3]) is not 'None':
-		if int(contents[2])<=delta and delta < int(contents[3]):
-			print contents
-	elif int(contents[2])<=delta:
-		print contents
+	contents = line.split(" ")[:-1]
+	#print contents
+	if int(contents[2])<=delta and delta < int(contents[3]):
+		contents[0]=map(int,contents[0].split(","))
+		contents[1]=map(int,contents[1].split(","))
+		contents[2:]=map(int,contents[2:])
+		cliques.append(Clique((set(contents[0]),(contents[4]-delta,contents[5]+delta),contents[1])))
+
+for c in cliques:
+	print c
