ee7968e87dffbb030868db7a42c76facd4d14749
Thibaud Arnoux <thibaud.arnoux@lip6.fr>
2014-11-28 14:16:25 +0100

    ajout noeud+temps

--- extendclique.py
+++ extendclique.py
@@ -12,25 +12,26 @@ cliquesliste=[]
 for line in sys.stdin:
 	contents = line.split(" ")[:-1]
 	#print contents
-	#if int(contents[2])<=delta and delta < int(contents[3]):
-	if int(contents[3]>delta):
+	if int(contents[2])<=delta and delta < int(contents[3]):
+	#if int(contents[3]>delta):
 		contents[0]=map(int,contents[0].split(","))
 		contents[1]=map(int,contents[1].split(","))
 		contents[2:]=map(int,contents[2:])
 		cliques.append(Clique((set(contents[0]),(contents[4]-delta,contents[5]+delta),contents[1])))
-		cliquesliste.append([set(contents[0]),(contents[4]-delta,contents[5]+delta),contents[1],len(set(contents[0]))])
+		cliquesliste.append([set(contents[0]),(contents[4]-delta,contents[5]+delta),contents[1],len(set(contents[0])),contents[5]+delta-(contents[4]-delta)])
 		
 print 'data loaded'
-for c in cliques:
-	print c
+
 #NB de noeud par clique
 histo=range(len(cliques))
 for i in range(len(cliques)):
 	histo[i]=len(cliques[i]._X)
 
-cliquesnbnodesmax=sorted(cliquesliste,key=operator.itemgetter(3))[-100:]
-for c in cliquesnbnodesmax:
-	print c[3],c[2][1]-c[2][0]
+cliquesnbnodesmax=sorted(cliquesliste,key=operator.itemgetter(3))[-3000:]
+cliquesnbnodesmaxtmpsmax=sorted(cliquesnbnodesmax,key=operator.itemgetter(4))[-50:]
+
+for c in cliquesnbnodesmaxtmpsmax:
+	print c[3],c[4]
 
 plt.subplot(211)
 valueshisto, basehisto=histogram(histo,bins=1000)
