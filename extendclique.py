6f5501c6aa4308494f62225a17797a34bb03c1ad
Thibaud Arnoux <thibaud@clevo.crans.org>
2014-12-01 15:56:34 +0100

    fixextend+ creation overlap

--- extendclique.py
+++ extendclique.py
@@ -22,12 +22,12 @@ cliquesliste=[]
 for line in sys.stdin:
 	contents = line.split(" ")[:-1]
 	#print contents
-	#if int(contents[2])<=delta and delta < int(contents[3]):
-	if int(contents[3])>delta:
+	if int(contents[2])<=delta and delta < int(contents[3]):
+	#if int(contents[3])>delta:
 		contents[0]=map(int,contents[0].split(","))
 		contents[1]=map(int,contents[1].split(","))
 		contents[2:]=map(int,contents[2:])
-                print contents
+                #print contents
 		cliques.append(Clique((set(contents[0]),(contents[4]-delta,contents[5]+delta),contents[1])))
 		cliquesliste.append([set(contents[0]),(contents[4]-delta,contents[5]+delta),contents[1],len(set(contents[0])),contents[5]+delta-(contents[4]-delta)])
 		
@@ -35,7 +35,15 @@ print 'data loaded'
 groscliques=findgroscliques(cliquesliste,4,10)
 for c in groscliques:
     print c
-
+groscliques=findgroscliques(cliquesliste,5,10)
+for c in groscliques:
+    print c
+groscliques=findgroscliques(cliquesliste,6,10)
+for c in groscliques:
+    print c
+groscliques=findgroscliques(cliquesliste,7,10)
+for c in groscliques:
+    print c
 
 #NB de noeud par clique
 histo=range(len(cliques))
@@ -47,6 +55,7 @@ valueshisto, basehisto=histogram(histo,bins=1000)
 cumulativehisto=cumsum(valueshisto)
 plt.plot(basehisto[:-1],cumulativehisto,c='blue')
 plt.title("Nombre de Clique a n noeud")
+
 
 #temps des cliques
 
