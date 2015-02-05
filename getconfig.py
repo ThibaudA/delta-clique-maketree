ef1e4f325f926d749eac5f07c38d21ef1c529f44
Thibaud Arnoux <thibaud.arnoux@lip6.fr>
2015-02-05 13:29:24 +0100

    echec on reprend du début. plsu d'inclusion, on recupere la trace

---
+++ getconfig.py
@@ -0,0 +1,11 @@
+from CliqueMaster import CliqueMaster
+from Clique import Clique
+from CliqueCritique import CliqueCritique
+from numpy import *
+from itertools import combinations
+from collections import deque
+import matplotlib.pyplot as plt
+import operator
+import sys
+
+
